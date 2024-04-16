f32825c21dab5a80204a8c58a5acf533a3ccb2ee
Seongjko <akth101@icloud.com>
2024-04-16 21:01:35 +0900

    changed one more variable env to data

--- exec/pre_processor.c
+++ exec/pre_processor.c
@@ -6,7 +6,7 @@
 /*   By: seongjko <seongjko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:15:57 by seongjko          #+#    #+#             */
-/*   Updated: 2024/04/16 20:58:52 by seongjko         ###   ########.fr       */
+/*   Updated: 2024/04/16 21:01:08 by seongjko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int pre_processor(t_list *finder, t_data *data, t_process *process)
 		return (0);
     if (!how_many_pipes(finder) && is_it_builtin_pre((t_cmd *)finder->content))
     {
-        builtin_handler((t_cmd *)finder->content, &env->env_head, process, env);
+        builtin_handler((t_cmd *)finder->content, &data->env_head, process, data);
         return (0);
     }
     return (1);
