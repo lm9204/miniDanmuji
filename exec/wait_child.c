08ac8b6ecb90d5da5cb756a91db1b97409ba481a
Seongjko <akth101@icloud.com>
2024-04-23 08:08:43 +0900

    signal handling after execve is now working(cat, wc ...)

--- exec/wait_child.c
+++ exec/wait_child.c
@@ -6,7 +6,7 @@
 /*   By: seongjko <seongjko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 17:02:25 by seongjko          #+#    #+#             */
-/*   Updated: 2024/04/19 03:32:02 by seongjko         ###   ########.fr       */
+/*   Updated: 2024/04/23 08:02:00 by seongjko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,13 @@ void	wait_child(int child_cnt, t_data *data)
 		{
 			exit_code = WEXITSTATUS(status);
 			data->exit_status = ft_itoa(exit_code);
+		}
+		else if (WIFSIGNALED(status))
+		{
+			if (WTERMSIG(status) == 2)
+				printf("\n");
+			else if (WTERMSIG(status) == 3)
+				printf("Quit: %d\n", WTERMSIG(status));
 		}
 		i++;
 	}
