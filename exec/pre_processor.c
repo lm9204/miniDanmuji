bf1f58cd736f87b68c320d4a7f1e4ca130c6e07e
yeondcho <yeondcho@student.42seoul.kr>
2024-04-30 22:05:54 +0900

    fix:  exit_status 누수 처리

--- exec/pre_processor.c
+++ exec/pre_processor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pre_processor.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seongjko <seongjko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:15:57 by seongjko          #+#    #+#             */
-/*   Updated: 2024/04/30 14:58:46 by seongjko         ###   ########.fr       */
+/*   Updated: 2024/04/30 16:15:08 by yeondcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	handle_builtin_without_pipe(t_list *finder, t_data *data)
 		return ;
 	exit_num = builtin_handler((t_cmd *)finder->content, \
 	&data->env_head, data);
+	free(data->exit_status);
 	data->exit_status = ft_itoa(exit_num);
 	dup2(backup.std_input, STDIN_FILENO);
 	dup2(backup.std_output, STDOUT_FILENO);
