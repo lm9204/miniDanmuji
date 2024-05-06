3179f6cf61e1d074f92545e22b512ebfe16473b6
yeondcho <yeondcho@student.42seoul.kr>
2024-05-06 14:22:49 +0900

    fix: parse_redirect norm 수정

--- parse/parse_redirect.c
+++ parse/parse_redirect.c
@@ -6,27 +6,27 @@
 /*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 17:28:38 by yeondcho          #+#    #+#             */
-/*   Updated: 2024/05/03 20:16:37 by yeondcho         ###   ########.fr       */
+/*   Updated: 2024/05/06 14:06:23 by yeondcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void redirect_arguments_error(char *cmd)
+static void	redirect_arguments_error(char *cmd)
 {
-    ft_putstr_fd(PROMPT_MSG, 2);
-    ft_putstr_fd(cmd, 2);
-    ft_putendl_fd(": ambiguous redirect", 2);
+	ft_putstr_fd(PROMPT_MSG, 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putendl_fd(": ambiguous redirect", 2);
 }
 
-char    *parse_redir(t_data *data, char *cmd, int *redir_flag)
+char	*parse_redir(t_data *data, char *cmd, int *redir_flag)
 {
-    char    *res;
+	char	*res;
 
-    res = checkcmd(data, cmd, redir_flag);
-    if (!res || res[0] == '\0')
-        *redir_flag = 1;
-    if (*redir_flag == 1)
-        redirect_arguments_error(cmd);
-    return (res);
+	res = checkcmd(data, cmd, redir_flag);
+	if (!res || res[0] == '\0')
+		*redir_flag = 1;
+	if (*redir_flag == 1)
+		redirect_arguments_error(cmd);
+	return (res);
 }
